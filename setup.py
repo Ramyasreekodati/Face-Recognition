e092c36f5644bfeb407fb113968904d9d15220e1
Ramyasreekodati <143927263+Ramyasreekodati@users.noreply.github.com>
2024-10-24 13:46:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+
+setup(
+    'project_name'="Face Recognition",
+    version=0.0.0,
+    author='Kodati ramya Sree',
+    author_email= Kodatiramyasree@gmail.com,
+    "github"=https://github.com/Ramyasreekodati,
+    description="""Project Description
+                The Face Recognition and Analysis System is an advanced application that utilizes deep learning and computer vision to identify, verify, and analyze human faces from images and video inputs. This project aims to provide an efficient, user-friendly interface for facial recognition, emotion detection, and demographic analysis.
+
+                Key Features
+                Face Recognition: Identifies similar faces in a database using the DeepFace library.
+                Face Verification: Confirms if two images are of the same person using cosine similarity.
+                Real-time Analysis: Analyzes video feeds for emotion, age, gender, and race.
+                User-Friendly Interface: Built with Gradio for seamless image uploads and instant feedback.
+                Technologies Used
+                TensorFlow: For deep learning model implementation.
+                DeepFace: Simplifies facial recognition tasks.
+                OpenCV: For video capture and frame processing.
+                Gradio: For creating an interactive web interface.
+                Applications
+                Security Systems: Enhancing identity verification processes.
+                Marketing: Analyzing customer demographics and emotions.
+                Research: Studying emotional responses and demographic data.
+
+                find_packages=Install_requires)""",
+
+
+)
